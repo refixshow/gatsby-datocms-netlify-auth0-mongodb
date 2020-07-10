502800900f5f475122982cde672c8db48e08323f
refixshow <adasscieszka@wp.pl>
2020-07-10 20:18:20 +0200

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
-import React from "react"
+import React, { useEffect } from "react"
+import Axios from "axios"
 
 export default function Home() {
+  useEffect(() => {
+    Axios.post("/.netlify/functions/sendEmail", {
+      message: "xddd",
+      name: "name",
+      email: "adamscieszka@gmail.com",
+    })
+  }, [])
+
   return <div>Hello world!</div>
 }
