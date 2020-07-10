bdd2a95341024a050cf7b292d976864a429340da
refixshow <adasscieszka@wp.pl>
2020-07-10 20:14:47 +0200

    a

--- functions/adam.js
+++
@@ -1,9 +0,0 @@
-exports.handler = function (event, context, callback) {
-  console.log(event, "event HERE")
-  console.log(context, "context HERE")
-
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World",
-  })
-}
