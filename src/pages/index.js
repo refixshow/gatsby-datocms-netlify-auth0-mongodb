14c09ba8d6bf4532666d726f316fbe468a1a7459
refixshow <adasscieszka@wp.pl>
2020-07-10 20:41:54 +0200

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,11 @@ import Axios from "axios"
 
 export default function Home() {
   useEffect(() => {
-    Axios.post(
-      "https://keen-meitner-56c2e9.netlify.app/.netlify/functions/sendEmail",
-      {
-        message: "xddd",
-        name: "name",
-        email: "adamscieszka@gmail.com",
-      }
-    )
+    Axios.post("/.netlify/functions/sendEmail", {
+      message: "xddd",
+      name: "name",
+      email: "adamscieszka@gmail.com",
+    })
       .then(res => {
         console.log(res)
       })
