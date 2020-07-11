bae207a26afd97df8456af31ad41d26688ca39bf
refixshow <adasscieszka@wp.pl>
2020-07-11 02:02:42 +0200

    xddd

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -1,9 +1,7 @@
 import nodemailer from "nodemailer"
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     type: "OAuth2",
     user: "adamscieszka@gmail.com",
@@ -11,9 +9,9 @@ const transporter = nodemailer.createTransport({
       "1094228178210-bjsgk40ru7p75nd40teu7307rgo8ssvr.apps.googleusercontent.com",
     clientSecret: "Xhl0t3Q8fFpRh5e2PXWB5FCp",
     refreshToken:
-      "1//04ZTAf13QMNc8CgYIARAAGAQSNwF-L9Ir4q0rstrKPzEpyyzvwGAYroB6meT02PvV0bmtt_uZdglbDO1JXe8QSqfHhZXVbZdQ8p4",
+      "1//047deFZ6dQ-6qCgYIARAAGAQSNwF-L9IrPmV7sE1mMHvo-pephGYioJXFwXLVkd2oHFKXq5KIn5cbjid8eCetfXyr7AH3HLO_J08",
     accessToken:
-      "ya29.a0AfH6SMDJ2cRBaM79zo8xsri4Mt_PauK2YKWUS0qSJ3dvYtH_1JVX5Mn8xcZTEEt4gHXbfvqjpFG9nnJokYqTo74R9lLWB1ZIJs6cEWD5Lev8PiInhpnSJsE6kzYIubjv0GewkkQ-F70is6dFzxSjo1OR-g8JYQOF-xo",
+      "ya29.a0AfH6SMANZwyu1-4Jo-zmdyNd8Bw3K7wuzmnSTrk6eJG3vCtyLpESL5QcC-Xzv7ziiCwgouGDYtz8SF-sYAv8-0tQwPekFRZj90zgI3j1FgB18Vm5bE9KHHDTw8A6wgFDRh5nOeM2H-rxp1SHIUPJvVTcwzY05-MtQ_s",
   },
 })
 
