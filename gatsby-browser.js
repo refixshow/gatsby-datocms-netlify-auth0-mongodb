41fd75a560f08f4dbb7e32d7fbc9780dd26aaba1
refixshow <adasscieszka@wp.pl>
2020-07-18 13:32:24 +0200

    b

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,7 @@ import { IdentityContextProvider } from "react-netlify-identity-widget"
 import "react-netlify-identity-widget/styles.css"
 
 export const wrapRootElement = ({ element }) => (
-  <IdentityContextProvider url="https://hungry-yonath-ed6433.netlify.com/">
+  <IdentityContextProvider url="https://keen-meitner-56c2e9.netlify.app/">
     {element}
   </IdentityContextProvider>
 )
