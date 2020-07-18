9cf70e7a984aa652aea0bd5a5ff58e9f74fc90a4
refixshow <adasscieszka@wp.pl>
2020-07-18 16:23:18 +0200

    b

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,6 +3,6 @@ import { IdentityContextProvider } from "react-netlify-identity-widget"
 import "react-netlify-identity-widget/styles.css"
 
 export const wrapRootElement = ({ element }) => {
-  const url = window.location.href
+  const url = "https://keen-meitner-56c2e9.netlify.app"
   return <IdentityContextProvider url={url}>{element}</IdentityContextProvider>
 }
