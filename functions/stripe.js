1a46857f983ddcf85543c5f576f40a879157ed40
refixshow <adasscieszka@wp.pl>
2020-07-18 17:20:09 +0200

    b

--- functions/stripe.js
+++ functions/stripe.js
@@ -1,3 +1,5 @@
+const { classroom } = require("googleapis/build/src/apis/classroom")
+
 const stripe = require("stripe")(
   "sk_test_51H4VmCK3hG2pI0lfwRGQwmgjB8xcXXOrn7yNxQpgmTOot6uRNczUu5imfaNV1nSPxy3GhT6SGPpejnN0WoLaCxdl00Ezz8JIhE"
 )
@@ -10,26 +12,36 @@ const headers = {
 }
 
 exports.handler = async function (event, context, callback) {
-  console.log(context.clientContext.user)
+  const claims = context.clientContext && context.clientContext.user
 
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        price: "price_1H4VpBK3hG2pI0lf0gD9G7qq",
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: "https://keen-meitner-56c2e9.netlify.app/success",
-    cancel_url: "https://keen-meitner-56c2e9.netlify.app/cancel",
-    customer_email: "adamscieszka@gmail.com",
-    locale: "pl",
-  })
+  if (claims) {
+    console.log(claims.user.email)
 
-  callback(null, {
-    statusCode: 200,
-    headers,
-    body: JSON.stringify({ sessionId: session.id }),
-  })
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: "price_1H4VpBK3hG2pI0lf0gD9G7qq",
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: "https://keen-meitner-56c2e9.netlify.app/success",
+      cancel_url: "https://keen-meitner-56c2e9.netlify.app/cancel",
+      customer_email: context.clientContext.user.email,
+      locale: "pl",
+    })
+
+    callback(null, {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ sessionId: session.id }),
+    })
+  } else {
+    callback(null, {
+      statusCode: 401,
+      headers,
+      body: "NOT LOGGED",
+    })
+  }
 }
