a638597b70d622802ebc09990c9c3d65447864c2
refixshow <adasscieszka@wp.pl>
2020-07-21 01:48:58 +0200

    woops

--- functions/googleCalendarDEV.js
+++ functions/googleCalendarDEV.js
@@ -72,7 +72,7 @@ exports.handler = async (event, context, callback) => {
           },
           (err, res) => {
             // Check for errors in our query and log them if they exist.
-            if (err) return console.error("Free Busy Query Error: ", err)
+            if (err) console.error("Free Busy Query Error: ", err)
 
             // Create an array of all events on our calendar during that time.
             const eventArr = res.data.calendars.primary.busy
@@ -80,26 +80,26 @@ exports.handler = async (event, context, callback) => {
             // Check if event array is empty which means we are not busy
             if (eventArr.length === 0)
               // If we are not busy create a new calendar event.
-              return calendar.events
+              calendar.events
                 .insert(
                   { calendarId: "primary", resource: eventCalendar },
                   err => {
                     // Check for errors and log them if they exist.
                     if (err)
-                      return console.error(
-                        "Error Creating Calender Event:",
-                        err
-                      )
+                      console.error("Error Creating Calender Event:", err)
                     // Else log that the event was created.
-                    return console.log("Calendar event successfully created.")
+                    console.log("Calendar event successfully created.")
                   }
                 )
                 .then(res => {
                   console.log(res)
                 })
+                .catch(err => {
+                  console.error(err)
+                })
 
             // If event array is not empty log that we are busy.
-            return console.log(`Sorry I'm busy...`)
+            console.log(`Sorry I'm busy...`)
           }
         )
 
